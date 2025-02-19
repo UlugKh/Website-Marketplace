d7e90317e4b19dae61fca48819609103ba968795
Bekmurod O'ktamov <bekmurod2510@gmail.com>
2025-02-19 17:12:45 +0300

    Interactive navbar
    I added 3 sections for. Each attached to a separate section. Now all we need is a design and images to make it complete.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,17 @@
+let navLinkEls = document.querySelectorAll('.nav_link');
+let sectionEls= document.querySelectorAll('section');
 
+let currentSection = 'home';
+window.addEventListener('scroll', () =>{
+    sectionEls.forEach(section => {
+        if(section.offsetTop < window.scrollY){
+            currentSection = section.id;
+        }
+    })
+    navLinkEls.forEach(link => {
+        link.classList.remove('active');
+        if(link.hash === `#${currentSection}`){
+            link.classList.add('active');
+        }
+    })
+})
